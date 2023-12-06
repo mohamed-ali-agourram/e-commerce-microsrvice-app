9b0e75bd99ecc5374425abdc5ff7388ee2f77b1d
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-06 11:12:38 +0100

    make connection to local mongodb database
    produit-service database

--- produit/index.js
+++ produit/index.js
@@ -2,6 +2,10 @@ console.clear()
 const express = require("express")
 const app = express()
 
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/produit-service")
+const Produit = require("./Produit")
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
